5203267f904f3a28447a138da29b5dfb5b7a948a
rachelharu <rachel.haru9@gmail.com>
2022-09-07 11:28:07 -0700

    user now able to enter search input which console logs results from api

---
+++ index.js
@@ -0,0 +1,66 @@
+// const fetchData = (searchTerm) => {
+//   const options = {
+//     method: 'GET',
+//     url: 'https://rawg-video-games-database.p.rapidapi.com/games',
+//     headers: {
+//       'X-RapidAPI-Key': '7813177d35mshc85ddf61935f917p12ead0jsn0e2bfb81f08e',
+//       'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
+//     },
+//     params: {
+//       key: 'bb7842b2785541ce8a8dd7522bac4816',
+//       search: searchTerm
+//     } 
+    
+//   };
+  
+//   axios.request(options).then(function (response) {
+//     console.log(response.data);
+//   }).catch(function (error) {
+//     console.error(error);
+//   });
+
+//   };
+  
+//   const input = document.querySelector('input');
+  
+//   let timeoutId;
+//   const onInput = event => {
+//     if (timeoutId) {
+//       clearTimeout(timeoutId);
+//     } 
+//     timeoutId = setTimeout(() => {
+//       fetchData(event.target.value);
+//     }, 450) 
+//   };
+  
+//   input.addEventListener('input', onInput);
+
+
+
+  const fetchData = async searchTerm => {
+    const response = await axios.get('https://rawg-video-games-database.p.rapidapi.com/games',
+    {
+      headers: {
+        'X-RapidAPI-Key': '7813177d35mshc85ddf61935f917p12ead0jsn0e2bfb81f08e',
+        'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
+      },
+      params: {
+        key: 'bb7842b2785541ce8a8dd7522bac4816',
+        search: searchTerm
+      }  
+    });
+  console.log(response.data.results);
+  };
+
+    
+    const input = document.querySelector('input');
+    
+    
+    const onInput = event => {
+      fetchData(event.target.value);
+    };
+    
+    input.addEventListener('input', debounce(onInput, 450));
+  
+  
+  
