05b0563dab3c2ecbb7cb70c61640136808606531
rachelharu <rachel.haru9@gmail.com>
2022-09-13 15:00:09 -0700

    add link to api database

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const fetchData = async (searchTerm) => {
       headers: {
         "X-RapidAPI-Key": "7813177d35mshc85ddf61935f917p12ead0jsn0e2bfb81f08e",
         "X-RapidAPI-Host": "rawg-video-games-database.p.rapidapi.com",
-         
       },
       params: {
         key: "bb7842b2785541ce8a8dd7522bac4816",
@@ -13,13 +12,12 @@ const fetchData = async (searchTerm) => {
       },
     }
   );
- 
-  
-  console.log(response.data.results.sort(sortBy('rating', true, parseInt)));
+
+  console.log(response.data.results.sort(sortBy("rating", true, parseInt)));
   return response.data.results;
 };
 
-const root = document.querySelector('.autocomplete');
+const root = document.querySelector(".autocomplete");
 root.innerHTML = `
   <label><b>Search</b></label>
   <input class="input" />
@@ -34,12 +32,11 @@ const input = document.querySelector("input");
 const dropdown = document.querySelector(".dropdown");
 const resultsWrapper = document.querySelector(".results");
 
-
 const onInput = async (event) => {
   const games = await fetchData(event.target.value);
-  
+
   if (!games.length) {
-    dropdown.classList.remove('is-active');
+    dropdown.classList.remove("is-active");
     return;
   }
 
@@ -48,16 +45,16 @@ const onInput = async (event) => {
   for (let game of games) {
     const option = document.createElement("a");
 
-    option.classList.add('dropdown-item');
+    option.classList.add("dropdown-item");
     option.innerHTML = `
         <img src="${game.background_image}" />
         <h1>${game.name}</h1>
         `;
-      option.addEventListener('click', () => {
-        dropdown.classList.remove("is-active");
-        input.value = game.name;
-        onGameSelect(game);
-      });
+    option.addEventListener("click", () => {
+      dropdown.classList.remove("is-active");
+      input.value = game.name;
+      onGameSelect(game);
+    });
 
     resultsWrapper.appendChild(option);
   }
@@ -65,40 +62,34 @@ const onInput = async (event) => {
 
 input.addEventListener("input", debounce(onInput, 450));
 
-
 //function to close dropdown menu if user clicks outside root"
-document.addEventListener("click", event => {
+document.addEventListener("click", (event) => {
   if (!root.contains(event.target)) {
-    dropdown.classList.remove('is-active');
+    dropdown.classList.remove("is-active");
   }
 });
 
-
 //reusable function to sort results by rating
 const sortBy = (field, reverse, primer) => {
+  const key = primer
+    ? function (x) {
+        return primer(x[field]);
+      }
+    : function (x) {
+        return x[field];
+      };
 
-  const key = primer ?
-  function(x) {
-    return primer(x[field])
-  } :
-  function(x) {
-    return x[field]
+  reverse = !reverse ? 1 : -1;
+
+  return function (a, b) {
+    return (a = key(a)), (b = key(b)), reverse * ((a > b) - (b > a));
   };
-
-reverse = !reverse ? 1 : -1;
-
-return function(a, b) {
-  return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
-}
 };
 
-
-
-const onGameSelect = game => {
-
+const onGameSelect = (game) => {
   console.log(game);
-  
-   document.querySelector("#summary").innerHTML = `
+
+  document.querySelector("#summary").innerHTML = `
    <article class="media">
      <figure class="media-left">
       <p class="image">
@@ -109,7 +100,7 @@ const onGameSelect = game => {
           <div class="content">
             <h1>${game.name}</h1>
             <h5>Genres: </h5>
-            <h4>${game.genres.map(o => o.name).join(", ")}</h4>
+            <h4>${game.genres.map((o) => o.name).join(", ")}</h4>
             <h5>ESRB: </h5>
             <h4>${game.esrb_rating.name}</h4>
           </div>
@@ -125,12 +116,9 @@ const onGameSelect = game => {
       </article>
       <article class="notification is-danger"> 
             <h5>Platforms: </h5>
-            <h4>${game.platforms.map(o => o.platform.name).join(", ")}</h4>
+            <h4>${game.platforms.map((o) => o.platform.name).join(", ")}</h4>
       </article>     
        
 
-       `
-     
-            
+       `;
 };
- 
