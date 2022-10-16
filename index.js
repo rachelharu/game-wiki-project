01a6434b9ee33d8f72402830ed65bdcbffddd73e
rachelharu <rachel.haru9@gmail.com>
2022-10-16 16:00:12 -0700

    add metacritic link

--- index.js
+++ index.js
@@ -31,12 +31,10 @@ const fetchDetails = async (id) => {
     }
   );
 
-  console.log(response.data.description_raw);
+  details(response.data);
+  console.log(response.data);
   return response.data.description_raw;
 };
-
-
-
 
 const root = document.querySelector('.autocomplete');
 root.innerHTML = `
@@ -49,7 +47,6 @@ root.innerHTML = `
   </div>
 `;
 
-
 const input = document.querySelector('input');
 const dropdown = document.querySelector('.dropdown');
 const resultsWrapper = document.querySelector('.results');
@@ -58,8 +55,6 @@ const subtitle = document.querySelector('.subtitle');
 const navInput = document.getElementById('nav');
 const autoComplete = document.getElementById('autoCom');
 const inputTag = document.querySelector('.inputTag');
-
-
 
 //creates dropdown menu, when user clicks option onGameSelect will run
 const onInput = async (event) => {
@@ -89,7 +84,8 @@ const onInput = async (event) => {
       navInput.appendChild(autoComplete);
       inputTag.classList.add('hide');
       input.classList.add('inputMargin');
-      onGameSelect(game); 
+      onGameSelect(game);
+      fetchDetails(game.id);
     });
 
     resultsWrapper.appendChild(option);
@@ -106,42 +102,45 @@ document.addEventListener('click', (event) => {
   }
 });
 
-// dsiplays results of game
+// passes game id to details function to show results
 const onGameSelect = async (game) => {
-  fetchDetails(game.id);
-  const yess = await fetchDetails(game.id);
   console.log(game);
+  details(id);
+};
+
+
+const details = async (id) => {
+  const description = id.description_raw;
+  const metaUrl = id.metacritic_url;
   document.querySelector('#summary').innerHTML = `
    <article class="media">
      <figure class="media-left">
       <p class="image">
-       <img id="art" src="${game.background_image}" />
+       <img id="art" src="${id.background_image}" />
        </p>
        </figure>
         <div class="media-content">
           <div class="content">
-            <h1>${game.name}</h1>
+            <h1>${id.name}</h1>
             <h5>Genres: </h5>
-            <h4>${game.genres.map((o) => o.name).join(', ')}</h4>
+            <h4>${id.genres.map((o) => o.name).join(', ')}</h4>
             <h5>ESRB: </h5>
-            <h4>${
-              game.esrb_rating === null ? 'N/A' : game.esrb_rating.name
-            }</h4>
+            <h4>${id.esrb_rating === null ? 'N/A' : id.esrb_rating.name}</h4>
           </div>
         </div>
     </article>
-            <h3>${yess }</h3>
+            <h3 id="description">${description.split('.', 2).join('.')}</h3>
       <article class="notification is-danger"> 
-            <h5>Metacritic Score: </h5>
-            <h4>${game.metacritic === null ? 'N/A' : game.metacritic}</h4>
+            <h5><a href=${metaUrl}>Metacritic Score:</a></h5>
+            <h4>${id.metacritic === null ? 'N/A' : id.metacritic}</h4>
       </article>
       <article class="notification is-danger"> 
             <h5>Released: </h5>
-            <h4>${game.released}</h4>
+            <h4>${id.released}</h4>
       </article>
       <article class="notification is-danger"> 
             <h5>Platforms: </h5>
-            <h4>${game.platforms.map((o) => o.platform.name).join(', ')}</h4>
+            <h4>${id.platforms.map((o) => o.platform.name).join(', ')}</h4>
       </article>     
        `;
 };
